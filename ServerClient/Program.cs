af14ab15b9ffeff66dde5c99d8206f16b664f8b3
glucaci <gabriel.lucaci.dev@gmail.com>
2016-12-14 13:18:58 +0100

    Client/Server test app

---
+++ ServerClient/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace ServerClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serverTask = Server.Start();
+
+            Client.Connect(Server.Address, Server.Port).GetAwaiter().GetResult();
+
+            serverTask.GetAwaiter().GetResult();
+        }
+    }
+
+
+    public class Client
+    {
+        public static async Task Connect(IPAddress address, int port)
+        {
+            var client = new TcpClient();
+            await client.ConnectAsync(address, port);
+        }
+    }
+
+    public class Server
+    {
+        public static IPAddress Address { get; } = IPAddress.Loopback;
+        public static int Port { get; } = 6600;
+
+        public static async Task Start()
+        {
+            var listner = new TcpListener(Address, Port);
+            listner.Start();
+
+            Console.WriteLine($"Listening on {Address}:{Port}");
+
+            await listner.AcceptTcpClientAsync();
+            Console.WriteLine("Client connected.");
+
+            Console.WriteLine("Shuting down...");
+            listner.Stop();
+        }
+    }
+}
---
+++ ServerClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Windows User")]
+[assembly: AssemblyProduct("ServerClient")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("58f6e155-0e68-4cb6-8763-398dae65db60")]
